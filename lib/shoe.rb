abecd296c3c2ccd717dfd48f4afd61daea5eb82f
Veronica Kim <vero.a.kim@gmail.com>
2018-02-08 23:24:51 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,6 +6,8 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+    BRANDS.include?(brand)
+    
   end
 
   def cobble
@@ -13,8 +15,8 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def brand=(brand)
-    @brand = brand
-    BRANDS << brand
-  end
+  # def brand=(brand)
+  #   @brand = brand
+  #   BRANDS << brand
+  # end
 end
